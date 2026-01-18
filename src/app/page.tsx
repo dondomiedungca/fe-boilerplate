aa1f1c6a3e14720158fafbc962aada9ed3b563d7
ddungca <41827126+dondomiedungca@users.noreply.github.com>
2026-01-18 21:15:32 +0800

    test the cookie for including on request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
 export default function Home() {
-  return <h4>This is boilerplate</h4>;
+  const handleFetch = () => {
+    fetch("http://localhost:3000/api", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+  };
+  return (
+    <>
+      <h4>This is boilerplate</h4>
+      <button onClick={handleFetch}>Click me</button>
+    </>
+  );
 }
